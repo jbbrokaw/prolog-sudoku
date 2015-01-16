b5d68742639c8a7346836796f3a6955056f6eb50
Jason Brokaw <jbrokaw@gmail.com>
2015-01-16 12:11:49 -0800

    Adds predicates to cycle through obvious solutions. Quickly solves easy puzzles

--- logic.pl
+++ logic.pl
@@ -77,8 +77,39 @@ filledObvious(Board):-
 	filledObviousRows(1, Board).
 
 
+/* Need a way to fill in obvious slots until there are none left: first
+need to count unknown items */
 
 
+numUnknownRow([], 0).
+numUnknownRow([X|Rest], N):-
+	var(X),
+	numUnknownRow(Rest, N2),
+	N is N2 + 1, !.
+numUnknownRow([X|Rest], N):-
+	nonvar(X),
+	numUnknownRow(Rest, N).
+
+numUnknown([], 0).
+numUnknown([Row|RestOfBoard], N):-
+	numUnknownRow(Row, NRow),
+	numUnknown(RestOfBoard, NRest),
+	N is NRow + NRest, !.
+
+/* Now fill obvious until numUnknown doesn't change */
+cycledObvious(_, 0).
+cycledObvious(Board, N):-
+	N > 0,
+	numUnknown(Board, NumOrig),
+	filledObvious(Board),
+	numUnknown(Board, NumNew),
+	N2 is NumOrig - NumNew,
+	writef("Unknowns: %w\n", [NumNew]),
+	cycledObvious(Board, N2), !.
+
+
+cycledObvious(Board):-
+	cycledObvious(Board, 1).
 
 
 
