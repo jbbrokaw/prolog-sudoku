a0b808f1b34bbe154df9d1b89736dd0db38d072b
Jason Brokaw <jbrokaw@gmail.com>
2015-01-15 18:18:27 -0800

    Adds predicates to find possibilities for unknown numbers

---
+++ logic.pl
@@ -0,0 +1,34 @@
+load_files(sudoku).
+
+/* For 'complement(Known, Possible)', known is
+a list of length 9, where some terms may be variables.
+Possiblities is the possibilities for each variable.
+*/
+complement([], [1,2,3,4,5,6,7,8,9]).
+complement([X|Rest], Possibilities):-
+	complement(Rest, Others),
+	nonvar(X),
+	select(X, Others, Possibilities).
+complement([X|Rest], Possibilities):-
+	complement(Rest, Others),
+	var(X),
+	Possibilities = Others.
+
+
+/* Possibilities/4 finds the possible values for
+the number at (RowNum, ColumnNum) of board.
+*/
+
+blockNum(N, RowNum, ColumnNum):-
+	N is  3 * (div(RowNum - 1, 3)) + div(ColumnNum - 1, 3) + 1.
+
+possibilities(Board, RowNum, ColumnNum, Possibilities):-
+	row(RowNum, Board, Row),
+	complement(Row, RowPossibilities),
+	column(ColumnNum, Board, Column),
+	complement(Column, ColumnPossibilities),
+	blockNum(BlockNum, RowNum, ColumnNum),
+	block(BlockNum, Board, Block),
+	complement(Block, BlockPossibilities),
+	intersection(RowPossibilities, ColumnPossibilities, RCPoss),
+	intersection(RCPoss, BlockPossibilities, Possibilities).
